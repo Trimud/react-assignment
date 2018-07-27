ac1442f01418883b8490db4f94ab208041a4eeef
Yuriy Boev <yuriy.boev@gmail.com>
2018-07-27 12:22:01 +0300

    Update Restaurant.js

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -87,7 +87,7 @@ class Restaurant extends React.Component {
           </Typography>
           <div className={classes.gmap}>
             <MapComponent
-              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyACteGNsEAWsbCvFgBG47KwQZ97aHcWKw4&v=3.exp&libraries=geometry,drawing,places"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=<API_KEY>&v=3.exp&libraries=geometry,drawing,places"
               loadingElement={<div style={{ height: `100%` }} />}
               containerElement={<div style={{ height: `300px` }} />}
               mapElement={<div style={{ height: `100%` }} />}
